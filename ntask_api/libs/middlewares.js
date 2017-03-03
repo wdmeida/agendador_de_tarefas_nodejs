cbdf5740a3e344d1ca3f5bf6f5c279b5e876448e
Wagner Almeida <wdmeida@gmail.com>
2017-03-03 14:53:00 -0300

    Implementação do crud de tarefas.

--- ntask_api/libs/middlewares.js
+++ ntask_api/libs/middlewares.js
@@ -1,4 +1,22 @@
+const bodyParser = require("body-parser");
+
 module.exports = app => {
   app.set("port", 3000);
   app.set("json spaces", 4);
+  app.use(bodyParser, json());
+  //Define um middleware global do Express para limpar o id das requisições.
+  app.use((res, res, next) => {
+    /*
+      Exclui o atributo id dentro do corpo de um requiquisição. Isso porque,
+      nas funções de cada requisição, usaremos o req.body como parâmetro das
+      funções do Sequelize, e o atributo req.body.id poderá sobrescrever o id
+      de uma tarefa, por exemplo, no update ou create.
+      Para finalizar o middleware, avisando que deve executar uma função respectiva
+      a um método do HTTP, basta incluir no final do callback a função next() para
+      ele avisar ao roteador do Express que ele pode executar a próxima função da rota
+      ou um próximo middleware abaixo.
+    */
+    delete req.body.id;
+    next();
+  });
 };
--- ntask_api/routes/tasks.js
+++ ntask_api/routes/tasks.js
@@ -1,8 +1,60 @@
 module.exports = app => {
   const Tasks = app.db.models.Tasks;
-  app.get("/tasks", (req, res) => {
-    Tasks.findAll({}).then(tasks => {
-      res.json({tasks: tasks});
+
+  app.route("/tasks")
+    .get((req, res) => {
+      //"/tasks": Lista de tarefas
+      Tasks.findAll({})
+        //O resultado da consulta ocorre por meio da função then().
+        .then(result => res.json(result))
+        //Caso exista algum problema, tratamos com a função catch().
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .post((req, res) => {
+      //"/tasks": Cadastra uma nova tarefa.
+      //A função Tasks.create faz uma limpeza dos campos que não existem no próprio modelo,
+      //então não há problemas em enviar o req.body diretamente.
+      Tasks.create(req.body)
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
     });
-  });
+
+  app.route("/tasks/:id")
+    .get((req, res) => {
+      //"/tasks/1" :Consulta uma tarefa específica.
+      //Procura uma tarefa específica através do seu id.
+      Tasks.findOne({where: req.params})
+        .then(result => {
+          if (result) {
+            res.json(result);
+          } else {
+            res.sendStatus(404);
+          }
+        })
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .put((req, res) => {
+      //"/task/1" :Atualiza uma tarefa específica.
+      //A função Tasks.update faz uma limpeza dos campos que não existem no próprio modelo,
+      //então não há problemas em enviar o req.body diretamente.
+      Tasks.update(req.body, {where: req.params})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        })
+    })
+    .delete((req, res) => {
+      //"/task/1" :Exclui uma tarefa específica.
+      Tasks.destroy({where: req.params})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    });
 };
